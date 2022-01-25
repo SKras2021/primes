4d9f1338e0599a3e599098ddfb6264c705654ce2
SKras2021 <85784408+SKras2021@users.noreply.github.com>
2022-01-25 20:38:24 +0100

    Add files via upload

--- WriteHTML.java
+++ WriteHTML.java
@@ -19,9 +19,11 @@ public class WriteHTML{
 		try{
 			FileWriter fw = new FileWriter("/Users/Dima/Desktop/primeVisual/test.html");
 			fw.write("<svg width = \""+ h + "\" height=\""+ h +"\" viewBox=\"0 0 "+ q +" "+ q +"\" version=\"1.2\" xmlns=\"http://www.w3.org/2000/svg\">\n");
-			for(int i = 0; i < x.size()-1; i++){
-				fw.write("<line x1=\""+ x.get(i) +"\" y1=\""+ y.get(i) +"\" x2=\""+ x.get(i+1) +"\" y2=\""+ y.get(i+1) +"\" style=\"stroke:rgb(20,20,20);stroke-width:2\"/>\n");
+			fw.write("<polyline points=\"");
+			for(int i = 0; i < x.size(); i++){
+				fw.write(x.get(i) +","+ y.get(i) + " ");
 			}
+			fw.write("\" style=\"fill:none;stroke:black;stroke-width:3\" />");
 			fw.write("</svg>\n");
 			fw.close();
 		} catch (IOException e){
